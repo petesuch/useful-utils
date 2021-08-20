a31a1d6746a05f7f47ae5481989ab40470b80e2c
Peter Suchsland <peter.suchsland@gmail.com>
2021-08-20 13:28:53 +0700

    Create getRidofMetadataGarbage.sh
    If you are transferring files from one USB drive to another, there's often a ton of metadata that gets put along with the source code.  And that turns a 10-minute file transfer into an all-night affair.   This is a dangerous script... so look it over before using it.

---
+++ getRidofMetadataGarbage.sh
@@ -0,0 +1,24 @@
+## getRidofMetadataGarbage.sh
+
+find . -type f -name *.class -delete
+find . -type f -name .metadata -delete
+find . -type d -name .metadata exec rm -rv {} +
+find . -type d -name *.metadata -exec rm -rv {} +
+find . -type f -name *.stackdump -delete
+find . -type d -name .git -exec rm -rv {} +
+find . -type f -name *.swp -delete
+find . -type f -name *.pyc -delete
+find . -type d -name .project -exec rm -rv {} +
+find . -type d -name .project
+find . -type d -name .py*
+find . -type d -name .classpath -exec rm -rv {} +;
+find . -type d -name .settings -exec rm -rv {} +
+find . -type f -name .classpath -exec rm -rv {} +
+find . -type f -name .tlog -exec rm -rv {} +
+find . -type f -name *.tlog -exec rm -rv {} +
+find . -type f -name .classpath -exec rm -rv {} +
+find . -type f -name *.cache -exec rm -rv {} +
+find . -type d -name .recommenders -exec rm -rv {} +
+find . -type f -name *.project -exec rm -rv {} +
+find . -type d -name *__pycache__ -delete
+find . -type d -name *__pycache__ -exec rm -rv {} +
